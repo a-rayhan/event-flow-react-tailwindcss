a001924d10148d172fc2f319b0701652da53a400
rayhan <aburayhandh@gmail.com>
2023-10-08 00:01:37 +0600

    Little design issue fixed and make 404 error page

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -10,11 +10,13 @@ import Login from "../Pages/Login/Login";
 import Register from "../Pages/Register/Register";
 import PrivateRoute from "../Pages/PrivateRoute/PrivateRoute";
 import ServicesDetails from "../Pages/ServicesDetails/ServicesDetails";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <MainLayout />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: '/',
